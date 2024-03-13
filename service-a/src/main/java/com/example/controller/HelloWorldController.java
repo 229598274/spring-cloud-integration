3213352436057510a927d4b62172bcc268938851
Administrator <admin@example.com>
2024-03-13 15:33:50 +0800

    ok

---
+++ service-a/src/main/java/com/example/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class HelloWorldController {
+    @GetMapping("/a")
+    public String getA(){
+        return "a";
+    }
+}
