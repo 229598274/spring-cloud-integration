d7ed189c2e52b2095b8a3e7ec90592e12a3e0d41
Administrator <admin@example.com>
2024-03-25 13:42:48 +0800

    ok

--- auth2-resource-starter/src/main/java/com/example/config/OAuth2ResourceServerSecurityConfiguration.java
+++ auth2-resource-starter/src/main/java/com/example/config/OAuth2ResourceServerSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,6 +32,8 @@ public class OAuth2ResourceServerSecurityConfiguration {
 
     @Bean
     JwtDecoder jwtDecoder() {
+        System.out.println("this.jwkSetUri");
+        System.out.println(this.jwkSetUri);
         return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
     }
 }
